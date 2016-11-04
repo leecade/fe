754cd358ea81ffffc650115cf4609e99bd9cf2df
leecade <leecade@163.com>
2016-11-04 18:24:14 +0800

    fixes standard code style

--- src/dev/start.js
+++ src/dev/start.js
@@ -1,19 +1,19 @@
-import detect from 'detect-port'
+// import detect from 'detect-port'
 
-detect(DEFAULT_PORT).then(port => {
-  if (port === DEFAULT_PORT) {
-    run(port)
-    return
-  }
+// detect(DEFAULT_PORT).then(port => {
+//   if (port === DEFAULT_PORT) {
+//     run(port)
+//     return
+//   }
 
-  clearConsole()
-  var question =
-    chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.') +
-    '\n\nWould you like to run the app on another port instead?'
+//   clearConsole()
+//   var question =
+//     chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.') +
+//     '\n\nWould you like to run the app on another port instead?'
 
-  prompt(question, true).then(shouldChangePort => {
-    if (shouldChangePort) {
-      run(port)
-    }
-  })
-})
+//   prompt(question, true).then(shouldChangePort => {
+//     if (shouldChangePort) {
+//       run(port)
+//     }
+//   })
+// })
